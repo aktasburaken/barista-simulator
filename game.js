1f5983c0baad96cb2eb339bf372c7fda1aea55cf
Burak <86871383+aktasburaken@users.noreply.github.com>
2022-07-26 16:46:06 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,76 @@
+var day = 1;
+console.log("------------------------ Day 1 --------------------------");
+setInterval(function () {
+    day += 1;
+    console.log("------------------------ Day ".concat(day, " --------------------------"));
+}, 10000);
+var Barista = /** @class */ (function () {
+    function Barista(name, speed, communication, skill) {
+        this.level = 1; // alttaki özellikelri artırıcak
+        this.madeCoffees = 0;
+        this.money = 0;
+        this.name = name;
+        if ((speed < 10 && communication < 10 && skill < 10) && (0 < speed && 0 < communication && 0 < skill)) {
+            this.speed = speed;
+            this.communication = communication;
+            this.skill = skill;
+        }
+        else {
+            console.log('Any property can not be less than 0 or more than 10.');
+        }
+    }
+    Barista.prototype.coffeePrepared = function () {
+        var _this = this;
+        setInterval(function () {
+            console.log("".concat(_this.name, "'s coffee ").concat(_this.madeCoffees));
+            if (_this.madeCoffees !== 0 && _this.madeCoffees % 10 === 0 && _this.level !== 10) {
+                _this.level += 1;
+                if (_this.speed <= 10) {
+                    _this.speed += 1;
+                    console.log("".concat(_this.name, "'s speed property is ").concat(_this.speed, " from now on."));
+                }
+                if (_this.communication <= 10) {
+                    _this.communication += 1;
+                    console.log("".concat(_this.name, "'s communication property is ").concat(_this.communication, " from now on."));
+                }
+                if (_this.skill <= 10) {
+                    _this.skill += 1;
+                    console.log("".concat(_this.name, "'s skill property is ").concat(_this.skill, " from now on."));
+                }
+                console.log("".concat(_this.name, " level upped to ").concat(_this.level));
+            }
+            else if (_this.level === 10) {
+                console.log("".concat(_this.name, " reached the max level of this game."));
+            }
+            _this.money += 0.10 * _this.communication;
+            console.log("".concat(_this.name, " prepared Coffee \u2615 ^^ \n").concat(_this.name, " has ").concat(_this.money.toFixed(2)));
+            _this.madeCoffees += 1;
+        }, 10000 / this.speed);
+    };
+    return Barista;
+}());
+var TalkativeBarista = new Barista('Jack', 3, 7, 5);
+var HighSkillBarista = new Barista('Emir', 4, 2, 8);
+var QuickBarista = new Barista('Burak', 8, 1, 4);
+TalkativeBarista.coffeePrepared();
+HighSkillBarista.coffeePrepared();
+QuickBarista.coffeePrepared();
+// class Customer {
+//     wantToTip: number; // tip atmak isteme oranı max10
+//     communication: number; //
+//     escapeBeforePay: number; // 0 max 10 min
+//     constructor(communication: number, wantToTip: number, escapeBeforePay: number) {
+//         if ((wantToTip < 10 && communication < 10 && escapeBeforePay < 10) && (0 < wantToTip && 0 < communication && 0 < escapeBeforePay)) {
+//             this.wantToTip = wantToTip
+//             this.communication = communication
+//             this.escapeBeforePay = escapeBeforePay
+//         } else {
+//             console.log('Any property can not be less than 0 or more than 10.')
+//         }
+//     }
+//     boughtCoffee(baristaSpeed: number, baristaCommunication: number, baristaSkill: number) {
+//         var coffeePreparingSpeed = baristaSpeed * this.communication
+//         var tipProspect = baristaCommunication * this.wantToTip
+//         var escapeProspect = baristaSkill * this.escapeBeforePay
+//     }
+// }
---
+++ game.ts
@@ -0,0 +1,100 @@
+
+let day = 1
+console.log(`------------------------ Day 1 --------------------------`)
+setInterval(() => {
+    day += 1
+    console.log(`------------------------ Day ${day} --------------------------`)
+}, 10000)
+
+const items = [{ name: 'shoe', price: 30 }, { name: 'shirt', price: 50 }, { name: 'Company stock', price: 40 }, { name: "Violin", price: 300 }, { name: "Tango Lesson", price: 40 }]
+
+class Barista {
+
+    private level: number = 1; // alttaki özellikelri artırıcak
+    name: string;
+    speed: number; // => daha çok kahve yapıcak
+    communication: number; // => tip alma yüzdesini artırıcak
+    skill: number; // => kahveyi daha güzel yapıcak
+    madeCoffees: number = 0;
+    money: number = 0;
+
+    constructor(name: string, speed: number, communication: number, skill: number) {
+
+        this.name = name
+
+        if ((speed < 10 && communication < 10 && skill < 10) && (0 < speed && 0 < communication && 0 < skill)) {
+            this.speed = speed
+            this.communication = communication
+            this.skill = skill
+        } else {
+            console.log('Any property can not be less than 0 or more than 10.')
+        }
+    }
+
+    coffeePrepared() {
+        setInterval(() => {
+            console.log(`${this.name}'s coffee ${this.madeCoffees}`)
+            if (this.madeCoffees !== 0 && this.madeCoffees % 10 === 0 && this.level !== 10) {
+                this.level += 1
+
+                if (this.speed <= 10) {
+                    this.speed += 1
+                    console.log(`${this.name}'s speed property is ${this.speed} from now on.`)
+                }
+                if (this.communication <= 10) {
+                    this.communication += 1
+                    console.log(`${this.name}'s communication property is ${this.communication} from now on.`)
+                }
+                if (this.skill <= 10) {
+                    this.skill += 1
+                    console.log(`${this.name}'s skill property is ${this.skill} from now on.`)
+                }
+
+                console.log(`${this.name} level upped to ${this.level}`)
+            } else if (this.level === 10) { return }
+
+            this.money += 0.10 * this.communication
+            console.log(`${this.name} prepared Coffee ☕ ^^ \n${this.name} has ${this.money.toFixed(2)}`)
+            this.madeCoffees += 1
+        }, 10000 / this.speed)
+
+        setInterval(() => {
+            console.log(`${this.name} broke a glass.`)
+            this.money -= 10 / this.skill
+            console.log(`${this.name} has ${this.money.toFixed(2)}`)
+        }, 10000)
+    }
+}
+
+const TalkativeBarista = new Barista('Jack', 3, 7, 5)
+const HighSkillBarista = new Barista('Emir', 4, 2, 8)
+const QuickBarista = new Barista('Burak', 8, 1, 4)
+
+TalkativeBarista.coffeePrepared()
+HighSkillBarista.coffeePrepared()
+QuickBarista.coffeePrepared()
+
+// class Customer {
+
+//     wantToTip: number; // tip atmak isteme oranı max10
+//     communication: number; //
+//     escapeBeforePay: number; // 0 max 10 min
+
+//     constructor(communication: number, wantToTip: number, escapeBeforePay: number) {
+//         if ((wantToTip < 10 && communication < 10 && escapeBeforePay < 10) && (0 < wantToTip && 0 < communication && 0 < escapeBeforePay)) {
+//             this.wantToTip = wantToTip
+//             this.communication = communication
+//             this.escapeBeforePay = escapeBeforePay
+//         } else {
+//             console.log('Any property can not be less than 0 or more than 10.')
+//         }
+//     }
+
+//     boughtCoffee(baristaSpeed: number, baristaCommunication: number, baristaSkill: number) {
+
+//         var coffeePreparingSpeed = baristaSpeed * this.communication
+//         var tipProspect = baristaCommunication * this.wantToTip
+//         var escapeProspect = baristaSkill * this.escapeBeforePay
+
+//     }
+// }
